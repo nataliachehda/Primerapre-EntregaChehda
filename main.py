3a96d2370acf894380fb6019fa9207ff32d0c265
nataliachehda <nataliachehda@gmail.com>
2023-06-01 14:44:48 -0300

    corrigiendo el mal cruzamiento de archivos

--- main.py
+++ main.py
@@ -2,59 +2,12 @@ import json
 import os
 from package1 import Cliente
 from package1.Cliente import Cliente
+from package1.Cliente import guardar_base_de_datos
 from package1.Cliente import base_de_datos
 from package1 import Celulares
 from package1.Celulares import Celulares
 
-# Pongo una línea separadora para que sea fácil de entender visualmente, porque el while repite el menú
-def separador():
-    return "_________________________________________"
-
-# Registrar un nuevo usuario o avisar si ya existe
-def registrar_usuario(usuario, contraseña, email, base_de_datos):
-    print(separador())
-    if usuario not in base_de_datos:
-        base_de_datos[usuario] = {"contraseña": contraseña, "email": email}
-        print(f"¡El usuario {usuario} ha sido registrado correctamente!")
-    else:
-        print(f"El usuario {usuario} ya existe en la base de datos.")
-
-# Mostrar los usuarios registrados (solo el usuario e email)
-def mostrar_usuarios(base_de_datos):
-    print(separador())
-    print("Usuarios registrados:")
-    for usuario, datos in base_de_datos.items():
-        print(f" - Usuario: {usuario}, Email: {datos['email']}")
-    print(separador())
-
-# Función para inicio de sesión 
-def iniciar_sesion(usuario, contraseña, base_de_datos):
-    if usuario in base_de_datos and base_de_datos[usuario]["contraseña"] == contraseña:
-        print(separador())
-        print(f"¡Bienvenid@ a π-thonicos, {usuario}! Pronto tendremos novedades para vos😎")
-        return True
-    else:
-        print(separador())
-        print("Usuario o contraseña incorrectos.")
-        return False
-
-# Cargar la base de datos desde un archivo JSON
-def cargar_base_de_datos():
-    try:
-        with open("base_de_datos.json", "r") as archivo:
-            return json.load(archivo)
-    except FileNotFoundError:
-        return {}
-
-# Guardo en el JSON
-def guardar_base_de_datos(base_de_datos):
-    with open("base_de_datos.json", "w") as archivo:
-        json.dump(base_de_datos, archivo)
-
-base_de_datos = cargar_base_de_datos()
-
-# Controlo el estado de inicio de sesión, para que si ya lo hizo no vuelva a pedir
-sesion_iniciada = False
+cliente = None  # Inicializar la variable cliente fuera del bucle while
 
 # Menú principal del programa
 while True:
@@ -100,5 +53,7 @@ while True:
         break
 
     else:
-        print(separador())
-        print("Opción inválida. Por favor, seleccione un número del 1 al 4.")
+        print("Opción inválida. Por favor, seleccione un número válido.")
+        input("Presione Enter para continuar...")
+
+guardar_base_de_datos (base_de_datos)
